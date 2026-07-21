96ea55305d7fb0448af03c15518d4dba7308baf2
Archu-ck <archismohakud5@gmail.com>
2026-07-21 08:42:16 +0530

    fix fs

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,17 +15,17 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
-app.use(cors({origin:FRONTEND_URL, credentials:true}));
-app.use(clerkMiddleware ());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
-app.get("/health", (req,res) => {
-  res.status(200).json({ok:true})
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true })
 });
 
-if(fs.existsSync(publicDir)){
+if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir))
 
-  app.get("*", (req,res,next) => {
+  app.get("{*path}", (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
@@ -34,7 +34,7 @@ app.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on PORT:", PORT)
 
-  if(process.env.NODE_ENV === "production"){
+  if (process.env.NODE_ENV === "production") {
     job.start();
   }
 });
