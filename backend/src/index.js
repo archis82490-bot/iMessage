f5da9db1b2f045bfeb007257b47c9d40e67da5ca
Archu-ck <archismohakud5@gmail.com>
2026-07-21 08:28:26 +0530

    add cronjob every 14 minutes and health check

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express';
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -31,4 +32,8 @@ if(fs.existSync(publicDir)){
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on PORT:", PORT)
+
+  if(process.env.NODE_ENV === "production"){
+    job.start();
+  }
 });
